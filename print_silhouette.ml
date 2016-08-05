69267cd2548915c9129f410ef3e7bd88e01e42e3
SimonBoulier <simon.boulier@ens-rennes.fr>
2016-08-05 15:27:37 +0200

    print_silhouette

--- print_silhouette.ml
+++ print_silhouette.ml
@@ -5,11 +5,13 @@ open BatIO
 open Graphics
        
 type point = num * num
-
+type segment = point * point
+			 
 type polygon = point list
 
 type silhouette = polygon list
-
+type skeleton = segment list
+			  
 let parse_num (s : string) : num =
   try
     let s1, s2 = BatString.split s "/" in
@@ -17,28 +19,36 @@ let parse_num (s : string) : num =
   with
   | Not_found -> of_string s
 			    
-			  
 let parse_point (s : string) : point =
   let s1, s2 = BatString.split s "," in
   parse_num s1, parse_num s2
+
+let parse_segment (s : string) : segment =
+  let s1, s2 = BatString.split s " " in
+  parse_point s1, parse_point s2
   
 
-(* todo: skeleton *)
-let parse_silouhette (filename : string) : silhouette =
+let parse_problem (filename : string) : silhouette * skeleton =
   let f = open_in filename in
+  (* parse silhouette *)
   let nb_poly = int_of_string (read_line f) in
   let silh = ref [] in
   for p = 1 to nb_poly do
     let poly = ref [] in
     let nb_vertices = int_of_string (read_line f) in
     for i = 1 to nb_vertices do
-      print_endline "ta";
       let v = parse_point (read_line f) in
       poly := !poly @ [v]
     done;
     silh := !silh @ [!poly]
   done;
-  !silh
+  (* parse skeleton *)
+  let nb_seg = int_of_string (read_line f) in
+  let skel = ref [] in
+  for p = 1 to nb_seg do
+    skel := !skel @ [parse_segment (read_line f)];
+  done;
+  !silh, !skel
 
   
 	    
@@ -47,29 +57,32 @@ let is_positive (p : polygon) : bool =
   true
 
 
-let width = 600
+let width = 950
 	      
 let print_polygon (sat : num * num -> int * int) (p : polygon) : unit =
   if is_positive p then
     set_color red
   else set_color white;
-  print_endline "t0";
-  let p' = BatArray.of_list p in
-  let p'' = Array.map sat p' in
-  print_endline "t0";
-  fill_poly p''
+  let p' = Array.map sat (BatArray.of_list p) in
+  fill_poly p'
 
+let print_segment (sat : num * num -> int * int) (s1, s2 : segment) : unit =
+  set_color black;
+  set_line_width 3;
+  let x,y = sat s1 in
+  let x', y' = sat s2 in
+  draw_segments [| x, y, x', y' |]
 	      
-let print_silhouette (s : silhouette) : unit =
+let print_problem (s, sk : silhouette * skeleton) : unit =
   let points = List.concat s in
   let xs = List.map fst points in
   let ys = List.map snd points in
   let minx, maxx = BatList.min_max ~cmp:compare_num xs in
   let miny, maxy = BatList.min_max ~cmp:compare_num ys in
-  let minx = min_num zero minx in
-  let maxx = max_num one maxx in
-  let miny = min_num zero miny in
-  let maxy = max_num one maxy in
+  let minx = minx - (of_float 0.01) in
+  let maxx = maxx + (of_float 0.01) in
+  let miny = miny - (of_float 0.01) in
+  let maxy = maxy + (of_float 0.01) in
   let maxmax = max_num (maxx - minx) (maxy - miny) in
   let scale_and_translate (x,y : num * num) : int * int =
     to_int (approx (((x - minx) * of_int width) / maxmax)),
@@ -83,14 +96,14 @@ let print_silhouette (s : silhouette) : unit =
   let sq = [| zero, zero; one, zero; one, one; zero, one |] in
   fill_poly (Array.map scale_and_translate sq);
   (* draw the polygons *)
-  List.iter (print_polygon scale_and_translate) s
+  List.iter (print_polygon scale_and_translate) s;
+  (* draw the skeleton *)
+  List.iter (print_segment scale_and_translate) sk;
+  (* press a key to exit *)
+  let _ = read_key () in
+  close_graph ()
+	    
 
-(* let p' = Array.map (fun (x,y) -> to_int (x * of_int width), to_int (y * of_int width)) *)
-		   (* (BatArray.of_list (BatList.hd s1)) *)
-	     
-
-
-let s1 = parse_silouhette "pb/3.pb"
 
 let () =
-  print_silhouette s1
+  print_problem (parse_problem "pb/16.pb")
