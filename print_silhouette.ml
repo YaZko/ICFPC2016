fa6e702ae0eac3c81ba5d6ea96683a60474d2d0b
SimonBoulier <simon.boulier@ens-rennes.fr>
2016-08-06 15:54:30 +0200

    translation and rotation

--- print_silhouette.ml
+++ print_silhouette.ml
@@ -13,10 +13,32 @@ type polygon = point list
 type silhouette = polygon list
 type skeleton = segment list
 
+type problem = silhouette * skeleton
+type solution = (point list) * (point list) * (int list list)
+
 let string_of_point (x, y : point) = (Num.to_string x) ^ "," ^ (Num.to_string y)
 let string_of_segment (p, q : segment) = "(" ^ (string_of_point p) ^ ";" ^ (string_of_point q) ^ ")"
 let string_of_polygon (l : polygon) = "[" ^ String.concat ";" (map string_of_point l) ^ "]"
 											 
+(* let string_of_problem (s, sk  *)
+
+let string_of_solution (points, dests, facets : solution) : string =
+  let s = output_string () in
+  write_line s (string_of_int (length points));
+  print ~first:"" ~last:"\n" ~sep:"\n"
+	(fun out point -> nwrite out (string_of_point point)) s points;
+  write_line s (string_of_int (length facets));
+  print ~first:"" ~last:"\n" ~sep:"\n"
+	(fun out poly ->
+	 nwrite s (string_of_int (length poly));
+	 print ~first:" " ~last:"" ~sep:" "
+	       (fun out n -> nwrite out (string_of_int n)) out poly)
+	s facets;
+  print ~first:"" ~last:"\n" ~sep:"\n"
+	(fun out point -> nwrite out (string_of_point point)) s dests;
+  close_out s
+
+	    
 let parse_num (s : string) : num =
   try
     let s1, s2 = String.split s "/" in
@@ -33,7 +55,7 @@ let parse_segment (s : string) : segment =
   parse_point s1, parse_point s2
   
 
-let parse_problem (filename : string) : silhouette * skeleton =
+let parse_problem (filename : string) : problem =
   let f = open_in filename in
   (* parse silhouette *)
   let nb_poly = int_of_string (read_line f) in
@@ -94,7 +116,7 @@ let print_segment (sat : num * num -> int * int) (s1, s2 : segment) : unit =
   let x', y' = sat s2 in
   draw_segments [| x, y, x', y' |]
 	      
-let print_problem (s, sk : silhouette * skeleton) : unit =
+let print_problem (s, sk : problem) : unit =
   let points = concat s in
   let xs = map fst points in
   let ys = map snd points in
@@ -118,30 +140,140 @@ let print_problem (s, sk : silhouette * skeleton) : unit =
   (* draw the skeleton *)
   List.iter (print_segment scale_and_translate) sk
 	    
-open Images
+(* let () = *)
+(*   open_graph ""; *)
+(*   set_window_title "youhou"; *)
+(*   resize_window width width; *)
+(*   for i = 228 to 690 do *)
+(*     clear_graph (); *)
+(*     print_problem (parse_problem ("pb/" ^ (string_of_int i) ^ ".pb")); *)
+(*     let img = get_image 0 0 width width in *)
+(*     Images.sauver_image (dump_image img) ("pb/" ^ (string_of_int i) ^ ".png"); *)
+(*   done; *)
+(*   close_graph () *)
+
+
+
+
+let id_pb : problem = parse_problem "pb/identity.pb"
+let id_sol : solution =
+  let [points], _ = id_pb in
+  points, points, [[0; 1; 2; 3]]
+
+type vector = num * num
+
+let inv (vx, vy : vector) : vector =
+  neg vx, neg vy
+	    
+let translate_point (vx, vy : vector) (px, py : point) : point =
+  px + vx, py + vy
+
+let minus (p2 : point) (p1 : point) : vector =
+  translate_point (inv p1) p2
+		  
+let translate_segment (v : vector) (p1, p2 : segment) : segment =
+  translate_point v p1, translate_point v p2
+    
+let translate_polygon (v : vector) (p : polygon) : polygon =
+  map (translate_point v) p
+
+
+type pb_transformer = (problem -> problem) * (solution -> solution)
+      
+let translate (v : vector) : pb_transformer =
+  let pb_tr (s, sk) =
+    map (translate_polygon v) s, map (translate_segment v) sk in
+  let sol_tr (points, dests, facets : solution) =
+    points, (map (translate_point (inv v)) dests), facets
+  in pb_tr, sol_tr
+
+(* rotation around the origin such that v goes on the x axe *)
+let rotate_point (vx, vy : vector) (px, py : point) : point =
+  let l = of_float (Float.sqrt (to_float (vx * vx + vy * vy))) in
+  (vx * px + vy * py) / l, (vx * py - vy * px) / l
+
+let rotate_segment (v : vector) (p1, p2 : segment) : segment =
+  rotate_point v p1, rotate_point v p2
+    
+let rotate_polygon (v : vector) (p : polygon) : polygon =
+  map (rotate_point v) p
+
+let symx (vx, vy : vector) : vector =
+  vx, neg vy
+      
+let rotate (v : vector) : pb_transformer =
+  let pb_tr (s, sk) =
+    map (rotate_polygon v) s, map (rotate_segment v) sk in
+  let sol_tr (points, dests, facets : solution) =
+    points, (map (rotate_point (symx v)) dests), facets
+  in pb_tr, sol_tr
+	    
+let left_bottom_point (s : silhouette) : point =
+  let points = concat s in
+  let minx, _ = min_max ~cmp:compare_num (map fst points) in
+  let points' = filter (equal minx % fst) points in
+  let miny, _ = min_max ~cmp:compare_num (map snd points') in
+  minx, miny
+
+let squared_norm ((p1x, p1y), (p2x, p2y) : segment) =
+  (p2x - p1x) * (p2x - p1x) + (p2y - p1y) * (p2y - p1y)
+	  
+let unit_length_segments_polygon (p : polygon) : segment list =
+  let segs = ref [] in
+  let verts = Array.of_list p in
+  let n = Array.length verts in
+  for i = 0 to pred n do
+    if equal (squared_norm (verts.(i), verts.(succ i mod n))) one then
+      segs := !segs @ [verts.(i), verts.(succ i mod n); verts.(succ i mod n), verts.(i)]
+  done;
+  !segs
+  
+let unit_length_segments (s : silhouette) : segment list =
+  concat (map unit_length_segments_polygon s)
+  
+	    
+let is_unit_square (p : polygon) =
+  eq (Tuple2.eq eq_num eq_num) p (hd (fst id_pb))
+	 
+let is_id_pb (s, sk : problem) =
+  match s with
+  | [p] -> is_unit_square p
+  | _ -> false
+
+let comp (t2 : pb_transformer) (t1 : pb_transformer) : pb_transformer
+  = (fst t2 % fst t1), (snd t1 % snd t2)
+	   
+
+let try_rotate (pb : problem) : solution =
+  let segs = unit_length_segments (fst pb) in
+  let transfs = map (fun (p1, p2) ->
+		     comp (rotate  (minus p2 p1)) (translate (inv p1)))
+		    segs in
+  print_int (length transfs); print_endline " ...";
+  (* print_string (string_of_polygon (hd (fst (hd (map fst pbs'))))); *)
+  let sols = filter (fun (pb_tr, sol_tr) -> is_id_pb (pb_tr pb)) transfs in
+  print_int (length sols); print_endline " ...";
+  snd (hd sols) id_sol
+  
+
 
 let () =
-  open_graph "";
-  set_window_title "youhou";
-  resize_window width width;
-  for i = 1 to 101 do
-    clear_graph ();
-    print_problem (parse_problem ("pb/" ^ (string_of_int i) ^ ".pb"));
-    let img = get_image 0 0 width width in
-    sauver_image (dump_image img) ("pb/" ^ (string_of_int i) ^ ".png");
-  done;
-  close_graph ()
-  (* let pb = (parse_problem ("pb/27.pb")) in *)
-  (* let p1 = hd (fst pb) in *)
-  (* let p2 = hd (tl (fst pb)) in *)
-  (* print_endline (string_of_polygon p1); *)
-  (* print_bool (is_positive p1); print_newline (); print_newline (); *)
-  (* print_endline (string_of_polygon p2); *)
-  (* print_bool (is_positive p2); print_newline (); print_newline (); *)
-  (* let pb = (parse_problem ("pb/sim")) in *)
-  (* let p1 = hd (fst pb) in *)
-  (* let p2 = hd (tl (fst pb)) in *)
-  (* print_endline (string_of_polygon p1); *)
-  (* print_bool (is_positive p1); print_newline (); *)
-  (* print_endline (string_of_polygon p2); *)
-  (* print_bool (is_positive p2); print_newline () *)
+  let pb = parse_problem "pb/102.pb" in
+
+  (* let pb', transf = translate (inv (left_bottom_point (fst pb))) pb in *)
+  (* print_string (string_of_solution (transf id_sol));; *)
+  print_string (string_of_solution (try_rotate pb))
+
+
+
+	       
+(* type point = num * num *)
+(* type segment = point * point *)
+			 
+(* type polygon = point list *)
+
+(* type silhouette = polygon list *)
+(* type skeleton = segment list *)
+
+(* type problem = silhouette * skeleton *)
+(* type solution = (point list) * (point list) * (int list list) *)
--- src/main.py
+++ src/main.py
@@ -1,40 +1,45 @@
-import solver,solver2
+# import solver,solver2
 import requests
 import json
 
 snap_hash = False
-get_snap = False
+get_snap = True
 headers = {'X-API-Key':'188-bba213ccaf3cb06f9f2db4a27a6cf380','Accept-Encoding':'gzip'}
 if snap_hash:
     r = requests.get('http://2016sv.icfpcontest.org/api/snapshot/list',headers=headers)
+    print (r.text)
     snapshothash = r.json()["snapshots"][-1]["snapshot_hash"]
 else:
-    snapshothash = "bbe7d278079a690055aab5a142688983fcf0d118"
+    snapshothash = "9023974d5b9aed37a1055311759e770a3809a96e"
 if get_snap:
     r = requests.get('http://2016sv.icfpcontest.org/api/blob/%s'%snapshothash,headers=headers)
     with open("snapshot.last","w") as fi:
         fi.write(r.text)
 
 snap = json.load(open("snapshot.last"))
+# print(snap)
 
-for pb_id in range(101):
+pbs = snap["problems"]
+
+for pb in pbs:
+    pb_id = pb["problem_id"]
     try:
-        pb_data = open("pb/%s.pb"%(pb_id+1)).read()
+        pb_data = open("pb/%s.pb"%(pb_id)).read()
     except Exception as e:
-        print("fetch pb",pb_id+1)
-        pb_hash = snap["problems"][pb_id]["problem_spec_hash"]
+        print("fetch pb", pb_id)
+        pb_hash = pb["problem_spec_hash"]
         r = requests.get('http://2016sv.icfpcontest.org/api/blob/%s'%pb_hash,headers=headers)
-# print(r.text)
+        # print(r.text)
         pb_data = r.text
-        with open("pb/%s.pb"%(pb_id+1),"w") as pb_file:
+        with open("pb/%s.pb"%(pb_id),"w") as pb_file:
             pb_file.write(pb_data)
 
-pb_id = 99 
-print(pb_id,"",end="")
-pb_data = open("pb/%s.pb"%pb_id).read()
+# pb_id = 99 
+# print(pb_id,"",end="")
+# pb_data = open("pb/%s.pb"%pb_id).read()
 
 #     solution = solver.solve(pb_data)
-solution = solver2.solve(pb_data)
+# solution = solver2.solve(pb_data)
 #     print("\n",solution)
 # content = gzip.decompress(r.content)
 # print(content)
