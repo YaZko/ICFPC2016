45c1ba616057a4378d74a2654a61574172721635
SimonBoulier <simon.boulier@ens-rennes.fr>
2016-08-07 15:33:58 +0200

    caml

--- caml/make_images.ml
+++ caml/make_images.ml
@@ -11,12 +11,16 @@ let () =
   open_graph "";
   set_window_title "youhou";
   resize_window width width;
-  for i = 1 to 2730 do
+  for i = 1 to 100 do
     clear_graph ();
     try
-      draw_problem (parse_problem ("../pb/convex/dontfit/" ^ (string_of_int i) ^ ".pb"));
+      draw_problem (parse_problem ("../pb/notconvex/hull/" ^ (string_of_int i) ^ ".pb"));
       let img = get_image 0 0 width width in
-      Images.sauver_image (dump_image img) ("../pb/convex/dontfit/" ^ (string_of_int i) ^ ".png");
+      Images.sauver_image (dump_image img) ("../pb/notconvex/hull/" ^ (string_of_int i) ^ ".png");
+      clear_graph ();
+      (* draw_problem (parse_problem ("../pb/notconvex/" ^ (string_of_int i) ^ "_hull.pb")); *)
+      (* let img = get_image 0 0 width width in *)
+      (* Images.sauver_image (dump_image img) ("../pb/notconvex/" ^ (string_of_int i) ^ "_hull.png"); *)
     with
     | _ -> ()
   done;
